a036b8f2b775ebe81d6d576fa9a2044190a5115a
tlras <14915607+tlras@users.noreply.github.com>
2022-02-04 22:35:17 -0600

    Add licence notice.

--- docs/m8k.js
+++ docs/m8k.js
@@ -7,6 +7,14 @@
 // @grant        none
 // ==/UserScript==
 
+/* Copyright 2021 tlras
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at:
+       http://www.apache.org/licenses/LICENSE-2.0
+*/
+
 ((w) => {
     // Global variables.
     let flight_enabled = false;
